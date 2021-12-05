8ee66e62476579fa9910adcf94a166d9d217f3dc
anastasiev <andrii.nastasiev@sigma.software>
2021-12-05 17:44:24 +0200

    add UI. add certificate with QR

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-const { PDFDocument, rgb, StandardFonts } = PDFLib
+const { PDFDocument, rgb } = PDFLib
 const fontkit = window.fontkit;
 
-const CONFIG = {
-    dila: {
+document.querySelector('.btn').addEventListener('click', () => modifyPdf());
+
+async function modifyPdf() {
+    const fields = document.forms[0].elements;
+
+    const url = `./${fields.template.value}.pdf`;
+    const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer())
+
+    const pdfDoc = await PDFDocument.load(existingPdfBytes);
+
+    const fontBytes = await fetch('./calibri.ttf').then((res) => res.arrayBuffer());
+    const boldFontBytes = await fetch('./calibri_bold.ttf').then((res) => res.arrayBuffer());
+
+    pdfDoc.registerFontkit(fontkit);
+    const font = await pdfDoc.embedFont(fontBytes);
+    const boldFont = await pdfDoc.embedFont(boldFontBytes);
+
+    const DILA_CONFIG = {
         name: {
             x: 357,
             y: 158,
             size: 10,
+            font: boldFont,
         },
         age: {
-            x: 455,
-            y: 175,
+            x: 456,
+            y: 176,
             size: 10,
         },
         sex: {
             x: 456,
-            y: 191,
+            y: 192,
             size: 10,
         },
         date: {
             top: {
-                x: 441,
-                y: 246,
+                x: 456,
+                y: 239,
                 size: 10,
             },
             bottom: {
@@ -30,83 +47,53 @@ const CONFIG = {
                 size: 8,
             },
         },
-    }
-};
+    };
 
-document.querySelector('.download').addEventListener('click', () => modifyPdf());
+    const CONFIG = {
+        dila: DILA_CONFIG,
+        dila_qr: DILA_CONFIG,
+    };
 
-
-async function modifyPdf() {
-    // Fetch an existing PDF document
-    const fields = document.forms[0].elements;
     const config = CONFIG[fields.template.value];
 
-    const url = `./${fields.template.value}.pdf`;
-    const existingPdfBytes = await fetch(url).then(res => res.arrayBuffer())
+    const pages = pdfDoc.getPages();
+    const firstPage = pages[0];
 
-    // Load a PDFDocument from the existing PDF bytes
-    const pdfDoc = await PDFDocument.load(existingPdfBytes);
-
-    // Embed the Helvetica font
-    const fontBytes = await fetch('./calibri.ttf').then((res) => res.arrayBuffer())
-
-
-    pdfDoc.registerFontkit(fontkit)
-    const font = await pdfDoc.embedFont(fontBytes)
-    // const font = await pdfDoc.embedFont(StandardFonts.TimesRoman);
-    // const boldFont = await pdfDoc.embedFont(StandardFonts.TimesRomanBold);
-
-    // Get the first page of the document
-    const pages = pdfDoc.getPages()
-    const firstPage = pages[0]
-
-    // Get the width and height of the first page
-    const { width, height } = firstPage.getSize()
-
-    const { name, date, sex } = document.forms[0].elements;
-
-    console.log( document.forms[0].elements);
-    console.log( firstPage.getSize());
-
-    // height: 841.89   792  280   0.34 2.94
-    // width: 595.276   612  210
-
-    // Draw a string of text diagonally across the first page
-    // firstPage.drawText('Dagestan!', {
-    //     x: 5,
-    //     y: height / 2 + 300,
-    //     size: 50,
-    //     font: helveticaFont,
-    //     color: rgb(0.95, 0.1, 0.1),
-    // })
+    const { height } = firstPage.getSize()
 
     Object.keys(config).forEach(key => {
         if (config[key].top) {
             firstPage.drawText(fields[key].value, {
+                font: font,
+                color: rgb(0, 0, 0),
                 ...config[key].top,
                 y: height - config[key].top.y,
+            });
+            firstPage.drawText(fields[key].value.split(' ')[0], {
                 font: font,
                 color: rgb(0, 0, 0),
-            });
-            firstPage.drawText(fields[key].value, {
                 ...config[key].bottom,
                 y: height - config[key].bottom.y,
-                font: font,
-                color: rgb(0, 0, 0),
             });
         } else {
             firstPage.drawText(fields[key].value, {
-                ...config[key],
-                y: height - config[key].y,
                 font: font,
                 color: rgb(0, 0, 0),
+                ...config[key],
+                y: height - config[key].y,
             });
         }
     });
 
-    // Serialize the PDFDocument to bytes (a Uint8Array)
+    firstPage.drawText(Date.now().toString().slice(0, 10), {
+        font: font,
+        color: rgb(0, 0, 0),
+        x: 456,
+        y: height - 223,
+        size: 10,
+    });
+
     const pdfBytes = await pdfDoc.save()
 
-    // Trigger the browser to download the PDF document
-    download(pdfBytes, "pdf-lib_modification_example.pdf", "application/pdf");
+    download(pdfBytes, `covid-certificate-${fields.date.value.split(' ')[0]}.pdf`, "application/pdf");
 }
